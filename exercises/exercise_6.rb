761fcf92070acbc13635d991686b94fe5d461683
JoshGrant5 <joshgg@icloud.com>
2020-11-09 21:59:55 +0000

    Used AR and store instances to add employees to employees db

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "J", last_name: "G", hourly_rate: 60)
+
+@store2.employees.create(first_name: "K", last_name: "V", hourly_rate: 60)
+
+@store2.employees.create(first_name: "H", last_name: "M", hourly_rate: 60)
+
+@store2.employees.create(first_name: "L", last_name: "P", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Q", last_name: "C", hourly_rate: 60)
+
+@store1.employees.create(first_name: "M", last_name: "N", hourly_rate: 60)
