b22eaf759deb574cc12ee8b059590d38f3094692
JoshGrant5 <joshgg@icloud.com>
2020-11-10 02:18:52 +0000

    Added before_destroy callback to check if store has employees before deleting

--- lib/store.rb
+++ lib/store.rb
@@ -3,8 +3,10 @@ class Store < ActiveRecord::Base
   has_many :employees
 
   validates :name, length: {minimum: 3}
-  validates :annual_revenue, numericality: { greater_than: 0, only_integer: true }
+  validates :annual_revenue, numericality: { greater_than_or_equal: 0, only_integer: true }
   validate :carries_apparel
+
+  before_destroy :check_employees
 
   def carries_apparel
     carry = 0;
@@ -13,6 +15,14 @@ class Store < ActiveRecord::Base
     end
     if carry == 0
       errors.add(:mens_apparel, "must carry at least one type of apparel")
+    end
+  end
+
+  private 
+  def check_employees
+    if self.employees.size > 0
+      errors.add(:employees, 'cannot destroy a store with employees')
+      return false;
     end
   end
 
